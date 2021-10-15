661ff0877b6a3b3a7c247ef2f0f2cb4660541fe3
Jason Calalang <jcalalang@ph.ibm.com>
2021-10-15 11:35:47 +0800

    Initial commit

---
+++ src/test/java/com/ibm/garage/sample/SampleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.garage.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
